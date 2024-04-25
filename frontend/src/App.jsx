91da227fd0a0249a99ed66ff3fabf0ccd23a981b
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2024-04-25 11:45:41 +0200

    Add Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className='loginMain'>
+      <h1 className='h1Logo'>CHECKMATE</h1>
+      <form action="POST" className='loginForm'>
+        <input type="text" placeholder='Username' className='inputUsername'/>
+        <input type="text" placeholder='Password' className='inputUsername'/>
+        <input type="button" value='LOGIN' className='loginBtn'/>
+      </form>
+      <a href="" className='signUpBtn'>Sign up</a>
+    </main>
   )
 }
 
